6e3956e606767e4586984b2b45883069dec758f5
ShiroTW <148338772+ShiroTW@users.noreply.github.com>
2025-05-21 21:52:01 +0300

    Add files via upload

---
+++ Lab12_2.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+#include <cassert>  // Для unit-тестів
+using namespace std;
+
+typedef int Info;
+
+struct Elem {
+    Elem* next;
+    Elem* prev;
+    Info info;
+};
+
+void enqueue(Elem*& first, Elem*& last, Info value) {
+    Elem* tmp = new Elem;
+    tmp->info = value;
+    tmp->next = NULL;
+
+    if (last != NULL)
+        last->next = tmp;
+    tmp->prev = last;
+    last = tmp;
+
+    if (first == NULL)
+        first = tmp;
+}
+
+void printList(Elem* L) {
+    while (L != NULL) {
+        cout << L->info << " ";
+        L = L->next;
+    }
+    cout << endl;
+}
+
+int countNegatives(Elem* L) {
+    int count = 0;
+    while (L != NULL) {
+        if (L->info < 0)
+            count++;
+        L = L->next;
+    }
+    return count;
+}
+
+void deleteList(Elem*& first) {
+    while (first != NULL) {
+        Elem* tmp = first->next;
+        delete first;
+        first = tmp;
+    }
+}
+
+int main() {
+    Elem* first = NULL;
+    Elem* last = NULL;
+
+    // Введення даних
+    enqueue(first, last, 4);
+    enqueue(first, last, -2);
+    enqueue(first, last, 0);
+    enqueue(first, last, -9);
+    enqueue(first, last, 7);
+    enqueue(first, last, -1);
+
+    cout << "List: ";
+    printList(first);
+
+    int negativeCount = countNegatives(first);
+    cout << "Number of negative elements: " << negativeCount << endl;
+
+    deleteList(first);
+    return 0;
+}
